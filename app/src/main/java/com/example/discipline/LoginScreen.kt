2760782794da958ccc07afae1c32702e797024e6
piotr <piotr.krolikowski2007@gmail.com>
2023-06-26 04:18:48 +0200

    .

---
+++ app/src/main/java/com/example/discipline/LoginScreen.kt
@@ -0,0 +1,107 @@
+package com.example.discipline
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen() {
+
+    var loginFieldState by remember { mutableStateOf("") }
+    var passwordFieldState by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.simple_logo),
+            contentDescription = null
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            modifier = Modifier
+                .width(370.dp),
+            value = loginFieldState,
+            onValueChange = { loginFieldState = it },
+            label = { Text("Email:") },
+            placeholder = { Text(text = "SamiecSigmaProgress123@hustler.cum") }
+        )
+
+        TextField(
+            modifier = Modifier
+                .width(370.dp),
+            value = passwordFieldState,
+            onValueChange = { passwordFieldState = it },
+            label = { Text("Password:") },
+            placeholder = { Text(text = "DailyGrind6969") }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {/*TODO*/ },
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            shape = RoundedCornerShape(28.dp),
+            modifier = Modifier
+                .width(350.dp)
+        ) {
+            Text(text = stringResource(R.string.login_with_psswd))
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {/*TODO*/},
+            shape = RoundedCornerShape(40.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            modifier = Modifier
+                .width(350.dp)
+        ) {
+            Box(){
+                Row() {
+                    Column(
+                        modifier = Modifier
+                            .height(20.dp),
+                        verticalArrangement = Arrangement.Center){
+                        Image(
+                            painter = painterResource(R.drawable.google_icon),
+                            contentDescription = null
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(10.dp))
+
+                    Text(text = stringResource(R.string.login_with_google))
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {/*TODO*/},
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            shape = RoundedCornerShape(28.dp),
+            modifier = Modifier
+                .width(350.dp)
+        ) {
+            Text(text = stringResource(R.string.sign_up))
+        }
+    }
+}
